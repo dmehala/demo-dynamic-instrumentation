4eef8d42c585491379d8a3b6ab92765219848972
Damien Mehala <damien.mehala@datadoghq.com>
2025-03-16 15:17:45 +0100

    add subfunctions

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def g():
+    return "<p>Hello, World!</p>"
+
+
+def f():
+    return g()
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return f()
